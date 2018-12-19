b424ca7e3598f66824eb5e0c05e600194fd64bfe
zgz19 <zgz19@DESKTOP-3F2TI5H>
2018-12-19 23:24:46 +0800

    111

---
+++ src/main/test/com/spring/in/action/c1/other/CDPlyaTest.java
@@ -0,0 +1,25 @@
+package soundsystem;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes=CDPlayerConfig.class)
+public class CDPlyaTest {
+	
+	@Autowired
+	private CompactDisc cd;
+	
+	@Test
+	public void cdShouldNotBeNull() {
+		assertNotNull(cd);
+		cd.play();
+	}
+	
+}
---
+++ src/main/test/com/spring/in/action/c1/other/SgtPeppers.java
@@ -0,0 +1,14 @@
+package soundsystem;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SgtPeppers implements CompactDisc {
+
+	@Override
+	public void play() {
+		// TODO Auto-generated method stub
+		System.out.println("play  SgtPeppers .....");
+	}
+
+}
